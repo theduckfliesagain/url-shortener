a94d6986a384c442cca508ae01075b9d986af987
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-11 16:18:43 +0100

    add form validation

--- shortener/urls/views.py
+++ shortener/urls/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from .forms import UrlForm
+from .models import Url
 
 def index(req):
+    if(req.method == "POST"):
+        form = UrlForm(req.POST)
+        if form.is_valid():
+            print(form.cleaned_data["long_url"])
+            url, created = Url.objects.get_or_create(long_url = form.cleaned_data["long_url"])
+            print(url, created)
+
+    else:
+        form = UrlForm()
     
-    return render(req, 'urls/index.html', {'form': UrlForm() })
+    return render(req, 'urls/index.html', {'form': form })
+
