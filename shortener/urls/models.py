7bc2b839fad27979ffe42405d760dc15d24bf081
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-11 16:18:55 +0100

    make urls unique

--- shortener/urls/models.py
+++ shortener/urls/models.py
@@ -1,9 +1,18 @@
 from django.db import models
-
+import random
 
 class Url(models.Model):
-    short_url = models.CharField(max_length=50)
-    long_url = models.CharField(max_length=200)
+    short_url = models.CharField(max_length=50, unique=True)
+    long_url = models.CharField(max_length=200, unique=True)
     
     def __str__(self):
         return self.short_url
+
+    # def save(self):
+    #     return super().save()
+
+    # @classmethod
+    # def shorten(long_url):
+    #     ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    #     return 
+
