797b352c726a9a624e689ffe5097d13193c54fe8
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-11 15:31:00 +0100

    add url model

---
+++ shortener/urls/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Url(models.Model):
+    short_url = models.CharField(max_length=50)
+    long_url = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
